9ee65e8fd189527814f28f348e451a77d9ae6fdf
Fabio9494 <fabio.casoria94@gmail.com>
2023-12-06 15:00:42 +0100

    add snack 2

--- js/script.js
+++ js/script.js
@@ -34,3 +34,48 @@ bikes.forEach((elem)=>{
 })
 
 console.log(`La bici piu leggera è la "${nomeBike}" e pesa ${pesoBike} kg`)
+
+// SNACK 2
+let squadre =[
+    {
+        nome: "Juventus",
+        falliSubiti: 0,
+        punti: 0
+    },
+    {
+        nome: "Inter",
+        falliSubiti: 0,
+        punti: 0
+    },
+    {
+        nome: "Milan",
+        falliSubiti: 0,
+        punti: 0
+    },
+    {
+        nome: "Roma",
+        falliSubiti: 0,
+        punti: 0
+    },
+    {
+        nome: "Atalanta",
+        falliSubiti: 0,
+        punti: 0
+    },
+]
+
+let falliSubitiPerSuadra=[];
+
+squadre.forEach((elem)=>{
+    elem.falliSubiti=Math.floor(Math.random() * 100);
+    elem.punti=Math.floor(Math.random() * 100);
+    
+    let {nome,falliSubiti}=elem;
+    let newItem={
+        nomeSquadra:nome,
+        falliSub:falliSubiti
+    }
+    falliSubitiPerSuadra.push(newItem);
+});
+    
+console.log(falliSubitiPerSuadra);
